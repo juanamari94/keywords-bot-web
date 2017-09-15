f3cb323c3180271a0eddcc6b38e48fbcd252977f
juan.jamari <juan.jamari@gmail.com>
2017-09-15 19:32:34 -0400

    Added API skeleton

---
+++ app/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const bodyParser= require('body-parser')
+const app = express()
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.listen(3000, function() {
+  console.log("Listening on port 3000");
+});
+
+app.get('/', (req, res) => {
+  res.status(200).send("Index");
+});
+
+app.get('/keys', (req, res) => {
+  res.status(200).send("/keys");
+});
+
+app.get('/keys/:id', (req, res) => {
+  res.status(200).send("/keys" + req.params.id);
+});
